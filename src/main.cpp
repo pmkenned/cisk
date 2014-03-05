7e1f23f1544c8d0ba0974f0886de1c45bbd0f2a7
Paul Kennedy <paul.kennedy124@gmail.com>
2014-03-04 23:51:14 -0600

    Made the MSVC solution use boost. Added some boost code in the main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
 #include <SDL.h>
 
+
+#include <boost/lambda/lambda.hpp>
+#include <iostream>
+#include <iterator>
+#include <algorithm>
+
 int main(int argc, char **argv){
+	using namespace boost::lambda;
+    typedef std::istream_iterator<int> in;
+
+    std::for_each(
+        in(std::cin), in(), std::cout << (_1 * 3) << " " );
+
 	if (SDL_Init(SDL_INIT_EVERYTHING) != 0){
 		std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
 		return 1;
